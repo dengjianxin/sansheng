444eb92790df7630b3e0c8cb9b290ffecaacdb48
dengjianxin <dengjianxin09@163.com>
2013-07-30 22:57:24 +0800

    qewq
    commit

--- snasheng/src/com/activity/CommonActivity.java
+++ snasheng/src/com/activity/CommonActivity.java
@@ -8,6 +8,7 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.example.sansheng.R;
 
+//xiugai  222222  
 public class CommonActivity extends SherlockFragmentActivity {
 	public static int THEME = R.style.Theme_Sherlock_Light;
 	TextView tvTitle;
